b12d9934166cd1a156af9f03aa54a1225deb9dab
Hiiamruss <130059165+Hiiamruss@users.noreply.github.com>
2024-05-25 09:52:07 +0700

    Add files via upload

---
+++ Pycount/Pycount.py
@@ -0,0 +1,19 @@
+"""Welcome to Pycount"""
+print("Hello World!")
+
+def add_num(A, B):
+    Ans = int(A) + int(B)
+    print(Ans)
+    print("Thank you for using Pycount!")
+
+#Start Num Test
+add_num(1, 2)
+
+def sub_num(A, B):
+    Ans = int(A) - int(B)
+    print(Ans)
+    print("Thank you for using Pycount!")
+
+#Start Sub Test
+sub_num(5, 1)
+
